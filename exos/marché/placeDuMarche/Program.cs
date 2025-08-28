27a0aca7b81bc83ea19e710ad7f19abef4c35769
chrisETML <christopher.ristic@yahoo.fr>
2025-08-28 08:50:58 +0200

    initial commit

---
+++ exos/marché/placeDuMarche/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Entreprise : ETML
+Auteur : Christopher Ristic 
+Date : 28.08.2025
+Description : Place du marché
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace placeDuMarche
+{
+    internal class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}

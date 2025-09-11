ebc1178e5eba06662bc8a7de2212b60e8424b434
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-09-11 10:52:24 +0200

    feat(mib): class Product

---
+++ exos/market-is-back/mib/Product.cs
@@ -0,0 +1,9 @@
+﻿internal class Product
+{
+    public UInt16 Location { get; set; }
+    public string Producer { get; set; }
+    public string ProductName { get; set; }
+    public UInt16 Quantity { get; set; }
+    public string Unit { get; set; }
+    public double PricePerUnit { get; set; }
+}

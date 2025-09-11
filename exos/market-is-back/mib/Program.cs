ec4bcc130dec5b470d8aa60cf441ba2b9022199b
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-09-11 12:14:27 +0200

    feat():fin

--- exos/market-is-back/mib/Program.cs
+++ exos/market-is-back/mib/Program.cs
@@ -1,5 +1,20 @@
 ﻿
-
+var i18n = new Dictionary<string, string>()
+{
+    { "Pommes","Apples"},
+    { "Poires","Pears"},
+    { "Pastèques","Watermelons"},
+    { "Melons","Melons"},
+    { "Noix","Nuts"},
+    { "Raisin","Grapes"},
+    { "Pruneaux","Plums"},
+    { "Myrtilles","Blueberries"},
+    { "Groseilles","Berries"},
+    { "Tomates","Tomatoes"},
+    { "Courges","Pumpkins"},
+    { "Pêches","Peaches"},
+    { "Haricots","Beans"}
+};
 
 List<Product> products = new List<Product>
 {
@@ -10,6 +25,20 @@ List<Product> products = new List<Product>
     new Product { Location = 2, Producer = "Dumont", ProductName = "Noix", Quantity = 20,Unit = "sac", PricePerUnit = 5.50 },
     new Product { Location = 2, Producer = "Dumont", ProductName = "Raisin", Quantity = 6,Unit = "kg", PricePerUnit = 5.50 },
     new Product { Location = 2, Producer = "Dumont", ProductName = "Pruneaux", Quantity = 13,Unit = "kg", PricePerUnit = 5.50 },
-    new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 },
+    new Product { Location = 2, Producer = "Dumont", ProductName = "Myrtilles", Quantity = 12,Unit = "kg", PricePerUnit = 5.50 }
+};
 
-}
+Func<Product, string> getProducer = x => $"{x?.Producer?[0]}{x?.Producer?[1]}{x?.Producer?[2]}...{x?.Producer?[x.Producer.Length - 1]}";
+Func<Product, string> getAliment = product => i18n[product.ProductName];
+Func<Product, double> chiffreAffaire = product => product.Quantity * product.PricePerUnit;
+
+string producerAliment = string.Join("\n", products.Select
+    (item => (
+    getProducer(item),
+    getAliment(item),
+    chiffreAffaire(item)
+    )
+));
+
+
+Console.WriteLine(producerAliment);

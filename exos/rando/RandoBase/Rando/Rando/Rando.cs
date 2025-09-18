9ef76c25dce8ae40ff6829243ef8182b279c5b3a
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-09-18 10:04:48 +0200

    fin rando

--- exos/rando/RandoBase/Rando/Rando/Rando.cs
+++ exos/rando/RandoBase/Rando/Rando/Rando.cs
@@ -1,10 +1,43 @@
+using Gpx;
+using System.Reflection.Metadata.Ecma335;
+
 namespace Rando
 {
     public partial class Rando : Form
     {
+        string gpxFile = @"Ballade_châtaignère.gpx";
+        List<Trackpoint> trackpoints = new();
         public Rando()
         {
             InitializeComponent();
+
+            if (!File.Exists(gpxFile))
+            {
+                MessageBox.Show("Pas trouve");
+            }
+
+            StreamReader streamReader = new StreamReader(gpxFile);
+
+            using (GpxReader reader = new GpxReader(streamReader.BaseStream))
+            {
+
+                while (reader.Read())
+                {
+                    switch (reader.ObjectType)
+                    {
+                        case GpxObjectType.Track:
+                            //writer.WriteTrack(reader.Track);
+                            GpxPointCollection<GpxPoint> gpxPoints = reader.Track.ToGpxPoints();
+
+                            // convertir les gpxpoint en trackpoint
+
+                            trackpoints.AddRange(gpxPoints.Select(p => new Trackpoint(p.Latitude * 10000, p.Longitude * 10000, p.Elevation)).ToList());
+
+
+                            break;
+                    }
+                }
+            }
         }
 
         private void Rando_Form_Paint(object sender, PaintEventArgs e)
@@ -12,8 +45,26 @@ namespace Rando
             Pen myPen = new Pen(Color.Red);
             myPen.Width = 2;
 
-            Point[] points = new Point[4] { new Point(30,50), new Point(50,10), new Point(80,50), new Point(111,400) };
+            var minLat = trackpoints.Min(p => p.Latitude);
+            var maxLat = trackpoints.Max(p => p.Latitude);
+            var rangeLat = maxLat - minLat;
+            var ratioLat = rangeLat / Width;
+
+            var minLong = trackpoints.Min(tp => tp.Longitude);
+            var maxLong = trackpoints.Max(tp => tp.Longitude);
+            var rangeLong = maxLong - minLong;
+            var ratioLong = rangeLong / Height;
+
+
+            Point[] points = trackpoints.Select(tp => new Point()
+            {
+                X = Convert.ToInt32((tp.Latitude - minLat) * ratioLat),
+                Y = Convert.ToInt32((tp.Longitude - minLong) * ratioLong)
+
+            }).ToArray();
+
             this.CreateGraphics().DrawLines(myPen, points);
+
         }
     }
 }
---
+++ exos/rando/RandoBase/Rando/Rando/Trackpoint.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Rando
+{
+    internal class Trackpoint
+    {
+        private double _latitude;
+        private double _longitude;
+        private double? _elevation;
+
+        public double Latitude
+        {
+            get => _latitude;
+            set => _latitude = value;
+        }
+        public double Longitude
+        {
+            get => _longitude;
+            set => _longitude = value;
+        }
+        public double? Elevation
+        {
+            get => _elevation;
+            set => _elevation = value;
+        }
+
+        public Trackpoint(double latitude, double longitude, double? elevation)
+        {
+            _latitude = latitude;
+            _longitude = longitude;
+            _elevation = elevation;
+        }
+        
+    }
+}

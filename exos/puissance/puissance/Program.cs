a52052f7fa04628f1fc652e7c736863b694dd658
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-30 09:57:24 +0100

    puissance

---
+++ exos/puissance/puissance/Program.cs
@@ -0,0 +1,12 @@
+﻿
+static double Power(double baseNumber, int exponent)
+{
+    if (exponent == 0)
+    
+        return 1;
+    
+
+   return baseNumber * Power(baseNumber, exponent - 1); // 32
+}
+
+Console.WriteLine(Power(2, 5));
